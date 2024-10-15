7044fa9e6fd5eebfb53f152e0b220e2bec237092
khori moore <khorimoore23@gmail.com>
2024-10-14 19:34:49 -0700

    added code to app.js

---
+++ AboutMe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section>
+      <h1>About Me</h1>
+      <p>Insert your bio here. Talk about your skills, experience, and what makes you unique!</p>
+    </section>
+  );
+}
+
+export default About;
---
+++ Resume.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Resume() {
+  return (
+    <section>
+      <h1>Resume</h1>
+      <p>Insert a link to download your resume here.</p>
+      <a href="/path-to-your-resume.pdf" download>Download My Resume</a>
+    </section>
+  );
+}
+
+export default Resume;
