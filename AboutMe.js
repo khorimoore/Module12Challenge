810246276f4dc13cb3a5de2ad2465e8a9a2bd118
khori moore <khorimoore23@gmail.com>
2024-10-14 19:53:20 -0700

    added code to aboutme.js

--- AboutMe.js
+++ AboutMe.js
@@ -4,7 +4,7 @@ function About() {
   return (
     <section>
       <h1>About Me</h1>
-      <p>Insert your bio here. Talk about your skills, experience, and what makes you unique!</p>
+      <p>Here's a little info about me as programmer. I have been coding for a few months now, specifically to apply the gained knowledge toward to My future Bionics and Nanotechnology programs I will be taking in the future. The experience in programming ive gained, and will gain in the future will allow me to create, for example "programmable tech at the nano scale"... such as micro drones or nanites!</p>
     </section>
   );
 }
